bdcef5e805617aa112403463f970d52335ed8652
Mikhail Gorbunov <mihail.grb@gmail.com>
2020-04-26 18:46:45 +0300

    Update request-card.component.ts

--- src/app/request-card/request-card.component.ts
+++ src/app/request-card/request-card.component.ts
@@ -140,7 +140,7 @@ export class RequestCardComponent implements OnInit, AfterViewInit {
           })
         },
         err => {
-          this.snackBar.open("Thomething was wrong", '', { duration: 2000 });
+          this.snackBar.open("Something was wrong", '', { duration: 2000 });
           console.log(err);
         }
       );
